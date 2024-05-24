6cdeef60f34836864bca8209cd9c6b559934a5f7
thiagorabaioli <tfrabaioli@outlook.com>
2024-05-24 14:46:38 +0100

    static method getUserInput()

--- App/src/App.java
+++ App/src/App.java
@@ -24,6 +24,28 @@ public class App {
 
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    Query q1 = new Query(
+        "consult",
+        new Term[] {new Atom("src/football_players.pl")}
+        );
+       // q1.hasMoreSolutions();
+        System.out.println( "consult " + (q1.hasSolution() ? "succeeded" : "failed"));
+
+        String positon = getUserInput();
+
+
+
     }
+    private static String getUserInput(){
+        String position="";
+        try(Scanner sc = new Scanner(System.in)){
+            System.out.println("Enter position");
+            position= sc.nextLine();
+        }
+        return position;
+
+    }
+
+
 }
