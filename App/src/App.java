6b4659293f1a85e95edabd38b057f23d0fa3060c
thiagorabaioli <tfrabaioli@outlook.com>
2024-05-24 14:40:23 +0100

    static class Player

--- App/src/App.java
+++ App/src/App.java
@@ -1,7 +1,28 @@
 
 import org.jpl7.*;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Scanner;;
+
 
 public class App {
+
+    static class Player{
+        private String name;
+        private String position;
+    
+    public Player(String name, String position){
+        this.name = name;
+        this.position = position;
+    }
+
+    public String getName(){ return name;}
+    
+    public String getPosition(){ return position;}
+
+}
+
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
